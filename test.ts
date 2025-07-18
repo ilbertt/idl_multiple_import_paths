d0f7ab75094a3aaf0f5515ac8b527af9e2243434
ilbertt <luca.bertelli@dfinity.org>
2025-07-18 23:19:46 +0200

    fix: patch `@dfinity/candid` from https://github.com/dfinity/agent-js/pull/1067

--- test.ts
+++ test.ts
@@ -1,16 +1,26 @@
 import { IDL as IDL1 } from "./idl_instance_1";
 import { IDL as IDL2 } from "./idl_instance_2";
 
-function test(): Uint8Array {
+function test(): [Uint8Array, Uint8Array] {
   const blobIDL1 = IDL1.Vec(IDL1.Nat8);
+  const blobIDL2 = IDL2.Vec(IDL2.Nat8);
 
   const encoded = IDL1.encode([blobIDL1], [[1, 2, 3]]);
-  const decoded = IDL2.decode([blobIDL1], encoded);
+  // this was fine even before the patch
+  const decoded = IDL2.decode([blobIDL2], encoded);
 
-  return decoded[0] as unknown as Uint8Array;
+  // this was broken before the patch
+  const decodedUsingIdl1Types = IDL2.decode([blobIDL1], encoded);
+
+  console.log({ decoded, decodedUsingIdl1Types });
+
+  return [
+    decoded[0] as unknown as Uint8Array,
+    decodedUsingIdl1Types[0] as unknown as Uint8Array,
+  ];
 }
 
 const result = test();
 
 console.log(result);
-// Should print out something like: Uint8Array(3) [ 1, 2, 3 ]
+// Should print out something like: [ Uint8Array(3) [ 1, 2, 3 ], Uint8Array(3) [ 1, 2, 3 ] ]
